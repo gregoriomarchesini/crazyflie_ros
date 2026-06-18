e880984d3124ca03af7aba307985004fb293d00a
Thibaud lallemant <pthibau@free.fr>
2026-06-18 13:07:31 +0200

    makefile to launch easily

---
+++ gen_makefile.bash
@@ -0,0 +1,44 @@
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+MAKEFILE_DIR=$(realpath "${SCRIPT_DIR}/../..")
+touch $MAKEFILE_DIR/Makefile
+cat > $MAKEFILE_DIR/Makefile << "EOF"
+SHELL := /bin/bash
+
+args := $(wordlist 2,$(words $(MAKECMDGOALS)),$(MAKECMDGOALS))
+map = $(filter %.yaml, $(args))
+ifeq ($(map), )
+#	By default, we run on the map test_real.yaml
+	map = test_real.yaml
+endif
+
+COLCON_STAMP := .colcon.stamp
+
+SRC = $(shell find src/ -name '*.py' -o -name '*.cpp')
+
+# Runs every only if a cpp or a python file is changed
+$(COLCON_STAMP) : $(SRC)
+	@ colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
+	@ source install/setup.bash
+	@ touch $@
+
+compile : $(COLCON_STAMP)
+
+simu : compile
+	@ ros2 launch crazyflie_ros2_setpoint_follower rvo.launch.py mission_yaml:=$(map) backend:=sim
+
+real_launcher :
+	@ ros2 launch crazyflie_ros2_setpoint_follower rvo.launch.py mission_yaml:=$(map) backend:=hardware
+
+real_hardware :
+#	sleep to be sure that this one is launched after the other one
+	@ gnome-terminal -- bash -c 'sleep 2; source /opt/ros/humble/setup.bash; source install/setup.bash; ros2 launch crazyflie_ros2_setpoint_follower launch_cf_hardware.launch.py; exit'
+
+real : compile
+	@ $(MAKE) -j 2 real_launcher real_hardware $(map)
+
+%::
+	@true
+
+EOF
+
+echo "Makefile generated"
